d1ae21f98229b41de6ff4f1f198bbed43b6b9c92
spencermountain <spencermountain@gmail.com>
2018-02-05 20:46:07 -0500

    use civil twilight as sunset time

--- data/index.js
+++ data/index.js
@@ -1,4 +1,4 @@
-module.exports = [
+let months = [
   require('./1'),
   require('./2'),
   require('./3'),
@@ -12,3 +12,18 @@ module.exports = [
   require('./11'),
   require('./12'),
 ]
+
+let all = []
+months.forEach((month) => {
+  month.forEach((day) => {
+    if (day[8]) {
+      all.push({
+        sunrise: day[8], //civil twilight
+        sunset: day[9],
+      })
+    }
+  })
+})
+
+console.log(JSON.stringify(all, null, 2))
+// console.log(all.length)
